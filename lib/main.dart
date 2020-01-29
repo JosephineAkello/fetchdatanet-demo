f339356293c62bf9b9d6c4d3becd0937ce5f96b2
Maureen Josephine <Maureen Josephine>
2020-01-29 22:36:53 +0300

    import dart convert

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
-Future<http.Response> fetchPost() {
-  return http.get('https://jsonplaceholder.typicode.com/posts/1');
+Future<Post> fetchPost() async {
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/posts/1');
+
+  if (response.statusCode == 200) {
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load post');
+  }
 }
-
 class Post {
   final int userId;
   final int id;
