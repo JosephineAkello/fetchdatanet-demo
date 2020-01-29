b8f8b3d35bf35c86737af9e4df034c6b679249b7
Maureen Josephine <Maureen Josephine>
2020-01-29 22:33:51 +0300

    add class model and fetchposts function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+Future<http.Response> fetchPost() {
+  return http.get('https://jsonplaceholder.typicode.com/posts/1');
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
 
 void main(){
   runApp(FetchData());
